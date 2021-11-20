eb3800732812ab18d2fe61187966921dd576e483
Alexis Rojas <alexRoJas_199@hotmail.com>
2021-11-20 00:20:23 -0600

    inclusion de graficas quadro y mejorado de decisiones

--- js/visibleelige.js
+++ js/visibleelige.js
@@ -40,7 +40,7 @@ var process=respuestaPROCE;
 var presup=document.getElementById("respuestapresup").value;
 var watts=document.getElementById("respuestawatts").value;
 
-if(tipo == "" || process == "" || presup == "" || watts == ""){
+if(tipo == null || process == "" || presup == "" || watts == ""){
     alert("Faltan datos");
     console.log(tipo);
     console.log(process);
@@ -62,8 +62,8 @@ if(presup < 2000 || watts < 400){
     if(tipo==1){
         if(process=="Intel"){
             if(model=="i2" || model=="i3" || model=="i4"){
-                if(presup >= 2000 && presup <= 5000){
-                    if(watts >= 400 && watts <= 600){
+                if(presup >= 2000 && presup < 5000){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/1030.jpg" id="imagenrecomendacion">te recomendamos la GT 1030';
                         
                     }
@@ -72,8 +72,8 @@ if(presup < 2000 || watts < 400){
         }
         if(process=="AMD"){
             if(model=="a2" || model=="a3" || model=="a4" || model=="a5"){
-                if(presup >= 2000 && presup <= 5000){
-                    if(watts >= 400 && watts <= 600){
+                if(presup >= 2000 && presup < 5000){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/1030.jpg" id="imagenrecomendacion">te recomendamos la GT 1030';
                     }
                 }
@@ -81,8 +81,8 @@ if(presup < 2000 || watts < 400){
         }
         if(process=="Intel"){
             if(model=="i2" || model=="i3" || model=="i4"){
-                if(presup >= 5000 && presup <= 6000){
-                    if(watts >= 400 && watts <= 600){
+                if(presup >= 5000 && presup < 6000){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/540.jpg" id="imagenrecomendacion">te recomendamos la RADEON 540';
                         
                     }
@@ -91,8 +91,8 @@ if(presup < 2000 || watts < 400){
         }
         if(process=="AMD"){
             if(model=="a2" || model=="a3" || model=="a4" || model=="a5"){
-                if(presup >= 5000 && presup <= 6000){
-                    if(watts >= 400 && watts <= 600){
+                if(presup >= 5000 && presup < 6000){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/540.jpg" id="imagenrecomendacion">te recomendamos la RADEON 540';
                     }
                 }
@@ -101,7 +101,7 @@ if(presup < 2000 || watts < 400){
         if(process=="Intel"){
             if(model=="i2" || model=="i3" || model=="i4"){
                 if(presup >= 6000){
-                    if(watts >= 400 && watts <= 600){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
                         
                     }
@@ -111,21 +111,21 @@ if(presup < 2000 || watts < 400){
         if(process=="AMD"){
             if(model=="a2" || model=="a3" || model=="a4" || model=="a5"){
                 if(presup >= 6000){
-                    if(watts >= 400 && watts <= 600){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
                     }
                 }
             }
         }
     }else if(tipo==2){
-        if(presup<6000){
-            alert("el presupuesto establecido no es suficiente para una tarjeta grafica de videojuegos")
+        if(presup<6000 || watts<400){
+            alert("el presupuesto establecido o la potencia en watts de su fuente de poder no es suficiente para una tarjeta grafica de videojuegos")
             break validacion;
         }
         if(process=="Intel"){
             if(model=="i2" || model=="i3" || model=="i4"){
-                if(presup >= 6000 && presup <= 10000){
-                    if(watts >= 400 && watts <= 600){
+                if(presup > 6000 && presup < 10000){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
                         
                     }
@@ -136,8 +136,8 @@ if(presup < 2000 || watts < 400){
         }
         if(process=="AMD"){
             if(model=="a2" || model=="a3" || model=="a4" || model=="a5"){
-                if(presup >= 6000 && presup <= 10000){
-                    if(watts >= 400 && watts <= 600){
+                if(presup > 6000 && presup < 10000){
+                    if(watts >= 400){
                         document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
                     }
                 } else if(model == "a2"){
@@ -147,39 +147,117 @@ if(presup < 2000 || watts < 400){
         }
         if(process=="Intel"){
             if(model=="i3" || model=="i4"){
-                if(presup >= 10000 && presup <= 30000){
-                    if(watts >= 400 && watts <= 800){
+                if(presup > 10000 && presup < 30000){
+                    if(watts >= 500){
                         document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
                         
+                    } else document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
+                }
+            }
+        }
+        if(process=="AMD"){
+            if(model=="a3" || model=="a4" || model=="a5"){
+                if(presup > 10000 && presup < 30000){
+                    if(watts >= 500){
+                        document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
+                    } else document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
+                }
+            }
+        }
+        if(process=="Intel"){
+            if(model=="i3" || model=="i4"){
+                if(presup > 30000){
+                    if(watts >= 600){
+                        document.getElementById("imag").innerHTML='<img src="../img/3070.jpg" id="imagenrecomendacion">te recomendamos la RTX 3070';
+                        
+                    }else if(watts >=500){
+                        document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
+                    } else document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
+                }
+            }
+        }
+        if(process=="AMD"){
+            if(model=="a3" || model=="a4" || model=="a5"){
+                if(presup > 30000){
+                    if(watts >= 600 && watts <= 800){
+                        document.getElementById("imag").innerHTML='<img src="../img/3070.jpg" id="imagenrecomendacion">te recomendamos la RTX 3070';
+                    } else if(watts >=500){
+                        document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
+                    } else document.getElementById("imag").innerHTML='<img src="../img/1050.jpg" id="imagenrecomendacion">te recomendamos la GTX 1050';
+                }
+            }
+        }
+    }else if(tipo==3){
+        if(presup<5000 || watts<500){
+            alert("el presupuesto establecido o la potencia de su fuente de poder no es suficiente para una tarjeta grafica para renderizado y/o modelado 3D")
+            break validacion;
+        }
+        if(process=="Intel"){
+            if(model=="i2" || model=="i3" || model=="i4"){
+                if(presup > 5000 && presup < 30000){
+                    if(watts >= 500){
+                        document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
+                        
+                    }
+                } else if(model=="i2"){
+                    document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
+                }
+            }
+        }
+        if(process=="AMD"){
+            if(model=="a2" || model=="a3" || model=="a4" || model=="a5"){
+                if(presup > 5000 && presup < 30000){
+                    if(watts >= 500){
+                        document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
+                    }
+                } else if(model == "a2"){
+                    document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
+                }
+            }
+        }
+        if(process=="Intel"){
+            if(model=="i3" || model=="i4"){
+                if(presup > 30000 && presup < 150000){
+                    if(watts >= 600){
+                        document.getElementById("imag").innerHTML='<img src="../img/QRTX4000.jpg" id="imagenrecomendacion">te recomendamos la QUADRO RTX 4000';
+                        
+                    }else if(watts >= 500 && watts <= 600){
+                        document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
                     }
                 }
             }
         }
         if(process=="AMD"){
             if(model=="a3" || model=="a4" || model=="a5"){
-                if(presup >= 10000 && presup <= 30000){
-                    if(watts >= 400 && watts <= 600){
-                        document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
+                if(presup > 30000 && presup < 150000){
+                    if(watts >= 600){
+                        document.getElementById("imag").innerHTML='<img src="../img/QRTX4000.jpg" id="imagenrecomendacion">te recomendamos la QUADRO RTX 4000';
+                    }else if(watts >= 500 && watts <= 600){
+                        document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
                     }
                 }
             }
         }
         if(process=="Intel"){
             if(model=="i3" || model=="i4"){
-                if(presup >= 30000){
-                    if(watts >= 600 && watts <= 800){
-                        document.getElementById("imag").innerHTML='<img src="../img/3070.jpg" id="imagenrecomendacion">te recomendamos la RTX 3070';
+                if(presup > 150000){
+                    if(watts >= 700){
+                        document.getElementById("imag").innerHTML='<img src="../img/QRTXA6000.jpg" id="imagenrecomendacion">te recomendamos la QUADRO RTX A6000';
                         
-                    }else document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
+                    }else if(watts >= 600 && watts < 700){ 
+                        document.getElementById("imag").innerHTML='<img src="../img/QRTX4000.jpg" id="imagenrecomendacion">te recomendamos la QUADRO RTX 4000';
+                    } else document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
                 }
             }
         }
         if(process=="AMD"){
             if(model=="a3" || model=="a4" || model=="a5"){
-                if(presup >= 30000){
-                    if(watts >= 600 && watts <= 800){
-                        document.getElementById("imag").innerHTML='<img src="../img/3070.jpg" id="imagenrecomendacion">te recomendamos la RTX 3070';
-                    } else document.getElementById("imag").innerHTML='<img src="../img/2060.jpg" id="imagenrecomendacion">te recomendamos la RTX 2060';
+                if(presup > 150000){
+                    if(watts >= 700){
+                        document.getElementById("imag").innerHTML='<img src="../img/QRTXA6000.jpg" id="imagenrecomendacion">te recomendamos la QUADRO RTX A6000';
+                    } else if(watts >= 600 && watts < 700){ 
+                        document.getElementById("imag").innerHTML='<img src="../img/QRTX4000.jpg" id="imagenrecomendacion">te recomendamos la QUADRO RTX 4000';
+                    } else document.getElementById("imag").innerHTML='<img src="../img/QT600.jpg" id="imagenrecomendacion">te recomendamos la QUADRO T600';
                 }
             }
         }
